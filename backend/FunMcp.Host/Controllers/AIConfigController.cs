8e5c876328f667ea3e4ae57db7421becb8deb71e
funshow <136590076@qq.com>
2025-04-12 20:05:32 +0800

    feat: add AIConfig Api

---
+++ backend/FunMcp.Host/Controllers/AIConfigController.cs
@@ -0,0 +1,25 @@
+﻿namespace FunMcp.Host.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class AIConfigController(IOptionsMonitor<AIOptions> aiOptions) : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<List<AIConfigDto>> Get()
+    {
+        var options = aiOptions.CurrentValue;
+       
+        var result = options.Configs.Select(x => new AIConfigDto
+        {
+            Name = x.Key,
+            Models = [.. x.Value.Models.Select(m => new AIModelDto
+            {
+                Name = m.Key,
+                ModelId = m.Value
+            })]
+        }).ToList();
+
+        return Ok(result);
+    }
+}
---
+++ backend/FunMcp.Host/Dto/AIConfigDto.cs
@@ -0,0 +1,15 @@
+﻿namespace FunMcp.Host.Dto;
+
+public class AIConfigDto
+{
+    public required string Name { get; set; }
+
+    public List<AIModelDto> Models { get; set; } = new ();
+}
+
+public class AIModelDto
+{
+    public required string Name { get; set; }
+
+    public required string ModelId { get; set; }
+}
