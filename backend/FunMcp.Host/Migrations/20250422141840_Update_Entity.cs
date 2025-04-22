296bd9e8d4bfc2b71567aa4abc8d20349ca2aaf1
funshow <136590076@qq.com>
2025-04-22 22:48:17 +0800

    add migrations files

---
+++ backend/FunMcp.Host/Migrations/20250422141840_Update_Entity.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FunMcp.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_Entity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxReconnectAttempts",
+                table: "McpServers");
+
+            migrationBuilder.DropColumn(
+                name: "ReconnectDelay",
+                table: "McpServers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "McpServerTools",
+                table: "AgentMcpServers",
+                type: "TEXT",
+                maxLength: 2048,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "McpServerTools",
+                table: "AgentMcpServers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxReconnectAttempts",
+                table: "McpServers",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ReconnectDelay",
+                table: "McpServers",
+                type: "INTEGER",
+                nullable: true);
+        }
+    }
+}
