0875684ca50e68b5e479433d72c8e47d701b82cb
刘俊繁_JF_L <liujunfan@vip.qq.com>
2025-06-12 12:35:50 +0800

    chore: 优化

--- backend/FunMcp.Host/_Global.cs
+++ backend/FunMcp.Host/_Global.cs
@@ -24,6 +24,7 @@ global using System.ClientModel;
 global using System.ComponentModel.DataAnnotations;
 global using System.Net.Mime;
 global using System.Security.Claims;
+global using System.Text;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
 global using Microsoft.AspNetCore.Http.HttpResults;
